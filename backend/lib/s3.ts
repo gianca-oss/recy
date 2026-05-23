1385ff8fa0cbbbf594fb86410d470ac11b5262ca
Giancarlo Corso <gianca.corso@gmail.com>
2026-05-23 11:53:55 +0200

    Switch S3 to virtual-hosted-style URLs
    Railway Object Storage (Tigris) requires virtual-hosted-style URLs.

--- backend/lib/s3.ts
+++ backend/lib/s3.ts
@@ -8,7 +8,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.S3_ACCESS_KEY!,
     secretAccessKey: process.env.S3_SECRET_KEY!,
   },
-  forcePathStyle: true,
+  forcePathStyle: false,
 });
 
 const BUCKET = process.env.S3_BUCKET!;
